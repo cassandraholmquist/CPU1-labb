80c286bfb2d455c04bc97658a289ab60af9fc45c
Cassandra.Holmquist <Cassandra.Holmquist@sigma.se>
2021-10-17 17:38:04 +0200

    package-lock.json

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+
+// View, Text, Image, Pressable (eller liknande), ScrollView (eller FlatList)
+// 3 egna komponenter varav minst en med props och minst en av dessa props ska vara en funktion (callback)
+// Använd dig av Context för att skicka ner viss data till komponenter utan att använda props. 
